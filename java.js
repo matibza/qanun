fa6d5470366aca63345d11503e83bafbc432e9c0
matibza <123789361+matibza@users.noreply.github.com>
2024-08-03 20:51:47 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,21 @@
+var contador = 0;
+
+
+function saludo() {
+  alert("Todavia estamos en construccion =), gracias por su paciencia.")
+}
+
+function banana() {
+    contador++
+var audio = document.getElementById("audio");
+var gg = document.getElementById("pl");
+    if (contador == 1) {
+      audio.play();
+      gg.innerHTML = "▉";
+    }
+    if (contador == 2) {
+      audio.pause();
+      contador = 0;
+      gg.innerHTML = "▶";
+    }
+}
